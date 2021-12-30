388e889fdffccb70f5ea529e3fe0d7817810c2bd
Brian Ai <brianai@gmail.com>
2021-12-29 19:50:16 -0800

    frog

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 
+void frogy() {
+    std::cout << "fregger";
+}
 
 int add(int a, int b) {
     return a + b;
@@ -16,4 +19,5 @@ int main() {
     std::cout << "Hello World!";
     hi();
     banana();
+    frogy();
 }
